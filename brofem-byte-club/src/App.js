2f4c623e10a244a48025c59cd1df6d01cf01a69a
nyaliti <bnyaliti@gmail.com>
2023-05-13 14:26:24 +0300

    updated package.json

--- brofem-byte-club/src/App.js
+++ brofem-byte-club/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import CreatePost from './components/CreatePost';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/create-post" component={CreatePost} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <Navbar />
+      <Home />
+      {/* Other components */}
+    </div>
   );
 };
 
